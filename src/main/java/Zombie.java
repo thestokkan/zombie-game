2462991e23aa5ea5c4c2adaf22042bf24341c7e1
Therese Stokkan <thestokkan@gmail.com>
2022-11-08 21:53:32 +0100

    Added constructor, getters and setters to Zombie

--- src/main/java/Zombie.java
+++ src/main/java/Zombie.java
@@ -13,4 +13,19 @@ public class Zombie {
     this.speed = speed;
   }
 
+  public int getX() {
+    return x;
+  }
+
+  public int getY() {
+    return y;
+  }
+  public void setX(int x) {
+    this.x = x;
+  }
+
+  public void setY(int y) {
+    this.y = y;
+  }
+
 }
