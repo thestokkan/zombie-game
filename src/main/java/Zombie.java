b274dfc9f042858b95bc92de18877ce2b902e52b
Maren Lilleberre <marenlilleberre@gmail.com>
2022-11-08 21:37:28 +0100

    add moveZombie method

--- src/main/java/Zombie.java
+++ src/main/java/Zombie.java
@@ -13,4 +13,24 @@ public class Zombie {
     this.speed = speed;
   }
 
+
+
+  // - Get player position -> send player position through moveZombie method
+  // - Calculate where(in what direction) to move
+  // - Move Zombie
+
+  public void moveZombie(int playerX, int playerY) {
+    int distanceX = x - playerX;
+    int distanceY = y - playerY;
+    //check distance regardless of position
+    if ( Math.abs(distanceX) > Math.abs(distanceY)) {
+      //check is x/y is pos or neg and move accordingly
+      x = distanceX>0 ? x- 1 : x + 1;
+    } else{
+      y = distanceY>0?y-1:y+1;
+    }
+  }
+
+
+
 }
