192e57317efd7aff91452f912be4417e3cba565a
Maren Lilleberre <marenlilleberre@gmail.com>
2022-11-09 12:35:41 +0100

    add addZombie method

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,6 +1,10 @@
 import java.util.ArrayList;
 
 public class Game {
-  Player player = new Player();
+  Player player = new Player(2,30,10);
   ArrayList<Zombie> zombies = new ArrayList<>();
+
+  public void addZombie(){
+    zombies.add(new Zombie(10,10));
+  }
 }
