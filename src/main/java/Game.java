b345e1721178b7750bff5028ce6ec4bddfe7f012
Maren Lilleberre <marenlilleberre@gmail.com>
2022-11-09 16:09:05 +0100

    imports

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,14 +1,13 @@
 
+  import com.googlecode.lanterna.SGR;
 import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+
 import java.io.IOException;
 import java.util.ArrayList;
-
-import com.googlecode.lanterna.SGR;
-import com.googlecode.lanterna.TextColor;
-import com.googlecode.lanterna.terminal.Terminal;
 
 public class Game {
   Player player = new Player(2, 30, 10);
